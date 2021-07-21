b688c8b05d9b001dcba53f45d2204cb903538685
Sean <smcgovern135@gmail.com>
2021-07-21 17:43:47 -0400

    new dependencies + app

--- client/index.js
+++ client/index.js
@@ -1,1 +1,27 @@
 // this file is a placeholder and can be modified to meet any needs
+
+import React, {useState, useEffect} from 'react';
+import reactDOM from 'react-dom';
+
+const App = () => {
+  //state hooks
+  const [text, updateText] = useState('');
+
+
+  //useEffect hooks
+  useEffect(() => {
+    updateText('world from useEffect');
+    //fetch
+  });
+
+  //return (like render)
+  return (
+    <div>
+      <p>Hello {text}</p>
+    </div>
+  );
+
+};
+
+reactDOM.render(<App />, document.getElementById('app'));
+export default App;
