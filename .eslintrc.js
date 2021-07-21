6a30a10cce9e9fb519882b6ccdd77824263f7450
Harshi <harshi.dandu88@gmail.com>
2021-07-21 16:39:59 -0500

    change eslint hr config to airbnb config

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: './node_modules/eslint-config-hackreactor/index.js',
-  rules: {
-    // Rules here will override the 'hackreactor' configuration
-    // http://eslint.org/docs/rules/
-  }
-};
