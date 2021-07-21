a99062b7dec601f5107d9b67537fe65241df9543
Frank G <fgauss3@gmail.com>
2021-07-21 18:25:04 -0400

    init

--- client/index.js
+++ client/index.js
@@ -1,1 +1,20 @@
-// this file is a placeholder and can be modified to meet any needs
+import React, {useState, useEffect} from 'react';
+import reactDOM from 'react-dom';
+
+const App = () => {
+  const [text, updateText] = useState('');
+
+
+  useEffect(() => {
+    updateText('World');
+  })
+
+  return (
+    <div>
+      <h1>Hello {text}!</h1>
+    </div>
+  )
+}
+
+reactDOM.render(<App />, document.getElementById('app'));
+export default app;
