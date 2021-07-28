dc934bb38b7ab2af9065f72cf36303ea563da067
Sean <smcgovern135@gmail.com>
2021-07-27 21:12:23 -0400

    added slide and arrow subcomponents

--- client/components/shared/Carousel.jsx
+++ client/components/shared/Carousel.jsx
@@ -1,20 +1,37 @@
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 
 const Car = styled.div``;
+const SlideImage = styled.div`
+  border: 1px solid black;
+  width: 60%;
+  height: 500px; 
+`;
 
+//slide subcomponent - image itself
 const Slide = ({ url }) => {
   return (
-    <div>
+    <SlideImage>
       <img src={url} alt="photo of clothing"/>
-    </div>);
-}
+    </SlideImage>);
+};
+
+//arrow subcomponent for nav
+const Arrow = ({direction}) => {
+  return (
+    <div>
+      {direction === 'Left' ? <AiOutlineArrowLeft /> : <AiOutlineArrowRight/>}
+    </div>); 
+};
 
 const Carousel = ({ urls }) => {
   const [index, setIndex] = useState(0); 
   return (
     <Car>
-       
+      <Arrow direction="Left"/>
+      <Slide url={urls[index]}/>       
+      <Arrow direction="Right" /> 
     </Car>);
 };
 
