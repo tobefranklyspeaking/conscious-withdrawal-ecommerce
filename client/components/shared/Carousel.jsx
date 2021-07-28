d035bd3d36a6655bf6a8fd1dd0961ef2cd95bcf7
Sean <smcgovern135@gmail.com>
2021-07-27 23:56:45 -0400

    added state-updating funcionality to arrows

--- client/components/shared/Carousel.jsx
+++ client/components/shared/Carousel.jsx
@@ -2,13 +2,20 @@ import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 
-const Car = styled.div``;
+//component styles
 const SlideImage = styled.div`
   border: 1px solid black;
   width: 60%;
   height: 500px; 
 `;
 
+const ArrowWrapper = styled.div`
+/*must add z index here*/
+`;
+
+const CarouselWrapper = styled.div`
+  display: flex;
+`;
 //slide subcomponent - image itself
 const Slide = ({ url }) => {
   return (
@@ -18,21 +25,35 @@ const Slide = ({ url }) => {
 };
 
 //arrow subcomponent for nav
-const Arrow = ({direction}) => {
+const Arrow = ({ direction, clickHandler}) => {
   return (
-    <div>
+    <ArrowWrapper onClick={clickHandler}>
       {direction === 'Left' ? <AiOutlineArrowLeft /> : <AiOutlineArrowRight/>}
-    </div>); 
+    </ArrowWrapper>); 
 };
 
 const Carousel = ({ urls }) => {
+  //state: index of images based on current item
   const [index, setIndex] = useState(0); 
+
+  //event handlers to switch carousel to next/previous image
+  const previousSlide = (e) => {
+    if (index >= 1) {
+      setIndex(index - 1);
+    }
+  };
+  const nextSlide = (e) => {
+    if (index < urls.length - 1) {
+      setIndex(index + 1);
+    }
+  };
+
   return (
-    <Car>
-      <Arrow direction="Left"/>
+    <CarouselWrapper>
+      <Arrow direction="Left" clickHandler={previousSlide}/>
       <Slide url={urls[index]}/>       
-      <Arrow direction="Right" /> 
-    </Car>);
+      <Arrow direction="Right" clickHandler={nextSlide}/> 
+    </CarouselWrapper>);
 };
 
 export default Carousel;
