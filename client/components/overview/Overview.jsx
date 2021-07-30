f8c270cfccec34905df0b40795f2f0011270023c
Sean <smcgovern135@gmail.com>
2021-07-29 21:59:28 -0400

    temp before pulling dev changes

--- client/components/overview/Overview.jsx
+++ client/components/overview/Overview.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
-
+import Carousel from '../shared/'
 //component styles
 const OverviewWrapper = styled.div`
   background-color: LightGray;
