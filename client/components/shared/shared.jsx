f1ce3acde5d2d963405dd56e76ae54d838b8a51f
Clendon DuRapau <clendon.durapau@gmail.com>
2021-07-31 11:55:45 -0500

    Renamed RelatedItems.js to RelatedItems.jsx to follow pattern of other components.

--- client/components/shared/shared.jsx
+++ client/components/shared/shared.jsx
@@ -3,7 +3,7 @@ import NavBar from './Navbar.jsx';
 import Overview from '../overview/Overview.js';
 import QandA from '../q&a/QandA.jsx';
 import Ratings from '../ratings-reviews/Ratings-reviews.js';
-import RelatedProducts from '../related-items/RelatedItems.js';
+import RelatedProducts from '../related-items/RelatedItems.jsx';
 
 const Shared = ({current}) => {
   return (
