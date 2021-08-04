671d51737518535a5e407076e701f64bc1ff4321
Harshi <harshi.dandu88@gmail.com>
2021-08-04 07:40:16 -0500

    refactor server/index.js

--- server/index.js
+++ server/index.js
@@ -9,44 +9,12 @@ app.use(express.static('public'));
 app.use(express.json());
 
 
-app.get('/*', (req, res) => {
+app.all('/*', (req, res) => {
   axios.defaults.headers.common['Authorization'] = API_KEY;
   axios({
     url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp${req.url}`,
     method: 'GET',
-    data: req.data || null
-  })
-    .then((response) => {
-      console.log('success with backend query');
-      res.status(200).send(response.data);
-    }).catch(err => {
-      console.log('failure with backend query');
-      res.status(400).send(err);
-    });
-})
-
-app.put('/*', (req, res) => {
-  axios.defaults.headers.common['Authorization'] = API_KEY;
-  axios({
-    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp${req.url}`,
-    method: 'PUT',
-    data: req.data || null
-  })
-    .then((response) => {
-      console.log('success with backend query');
-      res.status(200).send(response.data);
-    }).catch(err => {
-      console.log('failure with backend query');
-      res.status(400).send(err);
-    });
-})
-
-app.post('/*', (req, res) => {
-  axios.defaults.headers.common['Authorization'] = API_KEY;
-  axios({
-    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp${req.url}`,
-    method: 'POST',
-    data: req.body || null
+    data: req.data || req.body || null
   })
     .then((response) => {
       console.log('success with backend query');
