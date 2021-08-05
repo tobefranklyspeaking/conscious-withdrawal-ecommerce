1ebd555b499941cd76875df3a30aa03c8347c420
Sean <smcgovern135@gmail.com>
2021-08-05 01:28:31 -0400

    added space-evenly to flexbox and displayed state

--- client/components/shared/Modular-Carousel.jsx
+++ client/components/shared/Modular-Carousel.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 import { AiOutlineArrowLeft, AiOutlineArrowRight, AiOutlineArrowUp, AiOutlineArrowDown } from 'react-icons/ai';
 
-//higher order component: pass it an array of components to render, as well as an orientation ('row' or 'column')
-
+//higher order component: pass it an array of components to render, as well as an orientation ('row' or 'column'). It also expects a clickhandler (not for the arrows, but for the components themselves).
 //necessary styles
 const ArrowWrapper = styled.div`
   height: 15px;
@@ -25,19 +24,23 @@ const defaultComponents = [
 
 
 const ModularCarousel = ({ components=defaultComponents, orientation='row', clickHandler}) => {
+
+  //state for which components out of the total components prop to display
+  const [displayed, updateDisplayed] = useState(components);
+
   //this style inside component to allow for passing props
   const CarouselWrapper = styled.div`
     display: flex;
     flex-direction: ${orientation};
     align-items: center;
-    justify-content: center;
+    justify-content: space-evenly;
   `;
 
   //renders right set of arrows based on orientation string value
   return (
   <CarouselWrapper>
     <ArrowWrapper>{orientation === 'row' ? <AiOutlineArrowLeft /> : <AiOutlineArrowUp />}</ArrowWrapper>
-    {components}
+    {displayed}
     <ArrowWrapper>{orientation === 'row' ? <AiOutlineArrowRight /> : <AiOutlineArrowDown />}</ArrowWrapper>
   </CarouselWrapper>);
 };
