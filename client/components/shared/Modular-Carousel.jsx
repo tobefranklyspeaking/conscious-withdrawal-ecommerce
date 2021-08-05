252355d8a4f296b71744b88acb98f0e2bd7f5e07
Sean <smcgovern135@gmail.com>
2021-08-05 00:45:32 -0400

    initial commit

---
+++ client/components/shared/Modular-Carousel.jsx
@@ -0,0 +1,54 @@
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+import { AiOutlineArrowLeft, AiOutlineArrowRight, AiOutlineArrowUp, AiOutlineArrowDown } from 'react-icons/ai';
+
+
+const LeftArrowWrapper = styled.div`
+  height: 15px;
+  width: 15px;
+  position: absolute;
+  top: 50%;
+`;
+
+const RightArrowWrapper = styled.div`
+  height: 15px;
+  width: 15px;
+  position: absolute;
+  top: 50%;
+  left: calc(100% - 15px);
+`;
+
+const UpArrowWrapper = styled.div`
+  height: 15px;
+  width: 15px;
+  position: absolute;
+  left: 50%;
+`;
+
+const DownArrowWrapper = styled.div`
+  height: 15px;
+  width: 15px;
+  position: absolute;
+  top: calc(100% - 15px);
+  left: 50%;
+`;
+
+
+
+//higher order component: pass it an array of components to render, as well as an orientation ('row' or 'column')
+const mCarousel = ({ components=[], orientation='row'}) => {
+  const CarouselWrapper = styled.div`
+    display: flex;
+    flex-direction: ${orientation};
+  `;
+
+  return (
+  <CarouselWrapper>
+    {orientation === 'row' ?
+    <LeftArrowWrapper><AiOutlineArrowLeft /></LeftArrowWrapper> :
+    <UpArrowWrapper><AiOutlineArrowUp /></UpArrowWrapper>}
+    {components}
+  </CarouselWrapper>);
+};
+
+export default mCarousel;
