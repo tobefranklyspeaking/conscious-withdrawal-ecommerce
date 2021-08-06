fd4e563edd6e22b7ffbdfc1fd5d1a3df1e53a835
Clendon DuRapau <clendon.durapau@gmail.com>
2021-08-06 10:27:53 -0500

    Now toggles and saves or deletes outfits from My Outfit. However, does not re-render My Outfit until refresh.

--- client/components/related-items/ToggleOutfit.jsx
+++ client/components/related-items/ToggleOutfit.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import Star from '../shared/Star.jsx';
 
 export default ({ productID }) => {
+  var outfitList = JSON.parse(localStorage.getItem('myOutfit'));
   const [isFavorited, setIsFavorited] = useState();
 
   useEffect(() => {
-    var outfitList = JSON.parse(localStorage.getItem('myOutfit'));
     if (outfitList.includes(productID)) {
       setIsFavorited(true);
     } else {
@@ -13,10 +13,27 @@ export default ({ productID }) => {
     }
   }, [])
 
+  const handleClick = () => {
+    console.log('Toggle was clicked! 👀');
+    if (isFavorited) {
+      var productIndex = outfitList.indexOf(productID);
+      outfitList.splice(productIndex, 1);
+      outfitList = JSON.stringify(outfitList);
+      localStorage.setItem('myOutfit', outfitList);
+    } else {
+      outfitList.push(productID);
+      outfitList = JSON.stringify(outfitList);
+      localStorage.setItem('myOutfit', outfitList);
+    }
+    setIsFavorited(!isFavorited);
+  }
+
   return (
-    <Star
-    className='toggle-outfit star'
-    color={isFavorited ? 'gold' : 'hsla(0, 0%, 100%, .6)'}
-    />
+    <div onClick={handleClick}>
+      <Star
+      className='toggle-outfit star'
+      color={isFavorited ? 'gold' : 'hsla(0, 0%, 100%, .6)'}
+      />
+    </div>
   )
 }
