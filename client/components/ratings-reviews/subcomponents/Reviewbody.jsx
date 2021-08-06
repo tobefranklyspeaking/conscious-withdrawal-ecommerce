b74741afc4eda3a0796c542147a3eb4401bd1d6f
Harshi <harshi.dandu88@gmail.com>
2021-08-06 00:04:57 -0500

    implement photos in review body

--- client/components/ratings-reviews/subcomponents/Reviewbody.jsx
+++ client/components/ratings-reviews/subcomponents/Reviewbody.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
+import ReviewPhotos from './ReviewPhotos.jsx';
 
 
 const ReviewBodyWrapper = styled.div`
@@ -23,10 +24,28 @@ background-color: transparent;
   }
 `;
 
+const ImageContainer = styled.div`
+display: flex;
+justify-content: start;
+`;
+
+const Image = styled.img`
+border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 5px;
+  max-width: 7rem;
+max-height: 7rem;
+&:hover {
+  box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
+}
+`;
+
 const Reviewbody = ({body, photos}) => {
 
   const [bodyDisplay, setBodyDisplay] = useState("");
+  const [photoDisplay, setPhotoDisplay] = useState([photos]);
   const [showMore, setShowMore] = useState(false);
+  const [show, setShow] = useState(false);
 
 
 
@@ -41,20 +60,63 @@ useEffect(() => {
   }
 }, [body]);
 
+let handleImgClick = () => {
+  <Modal onClick={onClose}>
+      <ModalContent onClick={e => e.stopPropagation()}>
+        <ModalHeader>
+          <h4 className="modal-title">Write Your Review</h4>
+        </ModalHeader>
+        <ModalBody>
+          About the {index}
+        </ModalBody>
+        <ModalFooter className="modal-footer">
+          <button onClick={() => {onClose(); setCurrentIndex(null)}} className="button">Close</button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+
+
+};
 
 //console.log('reviewbody: ', bodyDisplay);
-//console.log('review photos', photos);
+console.log('review photos', photos);
 
   return (
     //
+    <>
     <ReviewBodyWrapper>
       {showMore ? bodyDisplay + '...' : bodyDisplay}
       {showMore &&
-      <ShowMoreButton className="ShowMoreButton" onClick={(e) => {setBodyDisplay(body); setShowMore(false);}}>ShowMore</ShowMoreButton>
+      <ShowMoreButton className="ShowMoreButton" onClick={(e) => {setBodyDisplay(body); setShowMore(false);}}>Show More</ShowMoreButton>
       }
     </ReviewBodyWrapper>
-
+    <ImageContainer>
+    {photos ? photos.map((pho, index) => (<><Image key={index} src={pho.url} alt="Review Image"/></>))  : '#'}
+      </ImageContainer>
+    </>
   );
 }
 
 export default Reviewbody;
+
+/*
+
+    <Image src="http://placecorgi.com/250" alt="corgi" />
+    <Image src="http://placecorgi.com/460/180" alt="corgi" />
+    <Image src="http://placecorgi.com/250" alt="corgi" />
+
+
+    {(photoDisplay.length > 0) &&
+    <ImageContainer>
+      photoDisplay.map((img) => (
+        <Image src={img} alt="image" />
+      ));
+    </ImageContainer>
+    }
+
+
+
+         {photos ? photos.map((pho, index) => (<><Image key={index} src={pho.url} alt="Review Image" onClick={() => setShow(true)}/></>))  : '#'}
+
+
+*/
