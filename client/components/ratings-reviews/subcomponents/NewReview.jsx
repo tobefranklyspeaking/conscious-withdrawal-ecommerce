7645ab04d170856acbafad5cbdc616c56657620a
Harshi <harshi.dandu88@gmail.com>
2021-08-06 10:14:54 -0500

    review form formatting

--- client/components/ratings-reviews/subcomponents/NewReview.jsx
+++ client/components/ratings-reviews/subcomponents/NewReview.jsx
@@ -18,20 +18,17 @@ const Modal = styled.div`
 `;
 
 const ModalContent = styled.div`
-  width: 80vh;
+  width: 90vh;
   height: 70vh;
   background-color: white;
-  border: 1px solid black;
 `;
 
 const ModalHeader = styled.div`
   padding: 10px;
-  border: 1px solid black;
 `;
 
 const ModalTitle = styled.div`
   margin: 0;
-  border: 1px solid black;
 `;
 
 const ModalBody = styled.div`
@@ -41,6 +38,8 @@ const ModalBody = styled.div`
   border bottom: 1px solid #eee;
   overflow: auto;
   border: 1px solid black;
+  font-weight: bold;
+  font-size: 25px;
 `;
 
 const ModalFooter = styled.div`
